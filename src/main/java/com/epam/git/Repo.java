1649a7743b7db943cb00f036f018a843e81ba203
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-16 15:29:54 +0300

    feat: solve

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/shapovalovaaa/stage2-module1-git-task";
 }
